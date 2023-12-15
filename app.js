2a257d1f3128e7a7d6fd8ac2faf9963946fd0b0a
Semanur Çakır <semanurcakir11@gmail.com>
2023-12-15 22:54:11 +0300

    The links have been updated.

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 import express from "express";
 const app = express();
-const port = 3000;
+//template engine
+app.set('view engine','ejs')
 
+//middlewares
+app.use(express.static('public'));
+
+//routes
 app.get("/", (req, res) => {
-  res.status(200).send("INDEX SAYFASI");
+  res.status(200).render('index',{
+    page_name:"index"
+  });
 });
+app.get("/about", (req, res) => {
+    res.status(200).render('about',{
+        page_name:"about"
+      });
+  });
 
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
