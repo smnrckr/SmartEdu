c647b67fd85bc7b20c17af9083a8df2db129b062
Semanur Çakır <semanurcakir11@gmail.com>
2023-12-15 22:29:57 +0300

    project created

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("INDEX SAYFASI");
+});
+
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
